52f0b98c8073e0979b9cfa1b8075ecf328a5919e
tofarias <tiago.farias.poa@gmail.com>
2019-07-24 10:50:26 -0300

    Add migration e model usuario

---
+++ database/migrations/2019_07_24_104513_create_usuarios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('usuario',40);
+            $table->string('email');
+            $table->string('password');
+            $table->boolean('verificado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
