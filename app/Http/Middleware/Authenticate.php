c0bc161332af8eac5e83d764178b3a9062d2ad18
tofarias <tiago.farias.poa@gmail.com>
2019-07-25 13:30:06 -0300

    Add exceptions

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,6 +15,14 @@ class Authenticate
             return response()->json(['token_expirado' => $e->getMessage()],500);
         }catch(\Tymon\JWTAuth\Exceptions\TokenInvalidException $e){
             return response()->json(['token_invalido' => $e->getMessage()],500);
+        }catch(\Tymon\JWTAuth\Exceptions\TokenBlacklistedException $e){
+            return response()->json(['token_blacklist' => $e->getMessage()],500);
+        }catch(\Tymon\JWTAuth\Exceptions\PayloadException $e){
+            return response()->json(['token_payload' => $e->getMessage()],500);
+        }catch(\Tymon\JWTAuth\Exceptions\InvalidClaimException $e){
+            return response()->json(['token_invalid_claim' => $e->getMessage()],500);
+        }catch(\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e){
+            return response()->json(['token_usuario_nao_definido' => $e->getMessage()],500);
         }catch(\Tymon\JWTAuth\Exceptions\JWTException $e){
             return response()->json(['token_ausente' => $e->getMessage()],500);
         }
